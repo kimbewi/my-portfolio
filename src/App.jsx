4de42867ef9a5d3bb2b60714cec23c2d5dcf22cd
kimbewi <kimberlytisip@gmail.com>
2026-06-16 19:14:35 +0800

    feat: create projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Hero />
         <About />
         <Skills />
+        <Projects />
       </main>
     </div>
   );
